ae896fc3a40edfa39c8884776094a1aa24cdbfcd
jcvic1 <victorjcdon@gmail.com>
2023-07-24 09:13:13 +0300

    First Commit

---
+++ database.py
@@ -0,0 +1,25 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from config import settings 
+
+
+
+POSTGRES_URL = f"{settings.POSTGRES_URL}"
+
+
+engine = create_engine(POSTGRES_URL, echo=True, pool_size=10, max_overflow=20)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
